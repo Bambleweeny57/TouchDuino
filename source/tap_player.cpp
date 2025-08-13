dc0f8196fcb79e54eb77ae1ca9652054a77794e1
Jon Gratton <jon@grattons.co.uk>
2025-08-13 20:02:12 +0100

    Update tap_player.cpp

--- source/tap_player.cpp
+++ source/tap_player.cpp
@@ -4,8 +4,15 @@
 void playTAP(File &file) {
     while (file.available()) {
         uint16_t blockLen = file.read() | (file.read() << 8);
+        uint32_t currentPos = file.position();
+        uint32_t totalSize = file.size();
+        drawProgressBar(currentPos, totalSize);
+
         for (uint16_t i = 0; i < blockLen; i++) {
             uint8_t byte = file.read();
+            uint32_t currentPos = file.position();
+            uint32_t totalSize = file.size();
+            drawProgressBar(currentPos, totalSize);
             for (int b = 7; b >= 0; b--) {
                 sendBit((byte >> b) & 1);
             }
