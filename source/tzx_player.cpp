4f5d9b191be9a2a9f7641c7b9a7c675e069a217e
Jon Gratton <jon@grattons.co.uk>
2025-08-13 19:47:32 +0100

    Update tzx_player.cpp

--- source/tzx_player.cpp
+++ source/tzx_player.cpp
@@ -43,3 +43,14 @@ void playTZX(File &file) {
         }
     }
 }
+
+void sendBit(bool bit) {
+#if ENABLE_CALIBRATION
+    digitalWrite(DATA_PIN, bit ? HIGH : LOW);
+    delayMicroseconds(PULSE_WIDTH_US);
+    digitalWrite(DATA_PIN, LOW);
+    delayMicroseconds(INTER_BIT_DELAY_US);
+#else
+    // Original TZX timing logic
+#endif
+}
