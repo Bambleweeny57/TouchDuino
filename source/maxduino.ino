25d9644e4c1e66b70f3e2d063ecc48fed3497cff
Jon Gratton <jon@grattons.co.uk>
2025-08-13 19:52:37 +0100

    Update maxduino.ino

--- source/maxduino.ino
+++ source/maxduino.ino
@@ -8,6 +8,8 @@
 #define MOTOR_PIN 2
 #define AUDIO_PIN 11
 
+#define MODE_CALIBRATION 99
+
 void setup() {
   Serial.begin(115200);
   pinMode(MOTOR_PIN, OUTPUT);
@@ -21,5 +23,35 @@ void loop() {
   if (!isPlaying()) {
     TouchAction action = detectTouchAction();
     handleTouchAction(action);
+
+    // Example dispatcher
+    switch (playMode) {
+      case MODE_TAP:
+        playTAP(file);
+        break;
+      case MODE_TZX:
+        playTZX(file);
+        break;
+      case MODE_CDT:
+        playCDT(file);
+        break;
+      case MODE_CAS:
+        playCAS(file);
+        break;
+      case MODE_TSX:
+        playTSX(file);
+        break;
+      case MODE_CALIBRATION:
+        runCalibration();
+        break;
+    }
   }
+}
+
+void runCalibration() {
+  Serial.println("Running signal calibration...");
+  for (int i = 0; i < 100; i++) {
+    sendBit(i % 2); // alternating bits
+  }
+  Serial.println("Calibration complete.");
 }
