ca5789f21393197bb7e584db4fca344ac5e1ff66
Jon Gratton <jon@grattons.co.uk>
2025-08-13 19:48:39 +0100

    Update tsx_player.cpp

--- source/tsx_player.cpp
+++ source/tsx_player.cpp
@@ -32,3 +32,13 @@ void playTSX(File &file) {
     }
 }
 
+void sendBit(bool bit) {
+#if ENABLE_CALIBRATION
+    digitalWrite(DATA_PIN, bit ? HIGH : LOW);
+    delayMicroseconds(PULSE_WIDTH_US);
+    digitalWrite(DATA_PIN, LOW);
+    delayMicroseconds(INTER_BIT_DELAY_US);
+#else
+    // TSX-specific logic
+#endif
+}
