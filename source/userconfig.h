17035434a8e552ea53dc5acd3704956310f9fa9f
Jon Gratton <jon@grattons.co.uk>
2025-08-13 12:12:35 +0100

    Create userconfig.h

---
+++ source/userconfig.h
@@ -0,0 +1,16 @@
+#ifndef USERCONFIG_H
+#define USERCONFIG_H
+
+// Enable TFT display
+#define USE_TFT
+
+// Disable OLED display
+#undef USE_OLED
+
+// Audio output pin
+#define AUDIO_PIN 9
+
+// Touchscreen CS pin
+#define CS_PIN 8
+
+#endif
