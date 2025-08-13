1953b801b2de21bc75e8076d3fb9afa000907df7
Jon Gratton <jon@grattons.co.uk>
2025-08-13 20:00:03 +0100

    Update cas_player.cpp

--- source/cas_player.cpp
+++ source/cas_player.cpp
@@ -4,7 +4,10 @@
 void playCAS(File &file) {
     while (file.available()) {
         uint8_t byte = file.read();
-
+        uint32_t currentPos = file.position();
+        uint32_t totalSize = file.size();
+        drawProgressBar(currentPos, totalSize);
+        
         // MSX CAS uses 1 start bit, 8 data bits, 1 stop bit
         sendBit(0); // Start bit
         for (int b = 0; b < 8; b++) {
