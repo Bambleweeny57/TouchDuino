3a8b540af4bf8e6e6a8700acdab04c70aab3b593
Jon Gratton <jon@grattons.co.uk>
2025-08-13 19:07:18 +0100

    Update cas_player.cpp

--- source/cas_player.cpp
+++ source/cas_player.cpp
@@ -1,10 +1,19 @@
-// cas_player.cpp
 #include "cas_player.h"
+#include "signal_generator.h"
 
 void playCAS(File &file) {
-  // CAS decoding logic
-  while (file.available()) {
-    byte b = file.read();
-    delayMicroseconds(100);
-  }
+    while (file.available()) {
+        uint8_t byte = file.read();
+
+        // MSX CAS uses 1 start bit, 8 data bits, 1 stop bit
+        sendBit(0); // Start bit
+        for (int b = 0; b < 8; b++) {
+            sendBit((byte >> b) & 1);
+        }
+        sendBit(1); // Stop bit
+
+        delayMicroseconds(100); // Inter-byte delay
+    }
+
+    delay(1000); // Final pause
 }
