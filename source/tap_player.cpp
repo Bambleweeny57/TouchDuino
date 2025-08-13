c51b163c83de872906be2653a72835855ebdd356
Jon Gratton <jon@grattons.co.uk>
2025-08-13 19:04:34 +0100

    Update tap_player.cpp

--- source/tap_player.cpp
+++ source/tap_player.cpp
@@ -1,10 +1,16 @@
-// tap_player.cpp
 #include "tap_player.h"
+#include "signal_generator.h"
 
 void playTAP(File &file) {
-  // Simple TAP playback logic
-  while (file.available()) {
-    byte b = file.read();
-    delayMicroseconds(100);
-  }
+    while (file.available()) {
+        uint16_t blockLen = file.read() | (file.read() << 8);
+        for (uint16_t i = 0; i < blockLen; i++) {
+            uint8_t byte = file.read();
+            for (int b = 7; b >= 0; b--) {
+                sendBit((byte >> b) & 1);
+            }
+        }
+        delay(1000); // Standard pause after block
+    }
 }
+
